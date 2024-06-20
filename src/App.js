c8bb4aae6f38869efdc056ba1d502a33afe17f0e
jin <junho11_kr@naver.com>
2024-06-21 08:11:25 +0900

    features

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Main from './pages/Main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import { createGlobalStyle } from 'styled-components';
+import FoodSidebar from './features/FoodSidebar';
+import Foodmain from './features/Foodmain';
 
 const GlobalStyle = createGlobalStyle`
   
@@ -28,6 +30,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Main/>}/>
           <Route path='login' element={<Login/>}/>
+
+          <Route path='foodsidebar' element={<FoodSidebar />}>
+              <Route path='foodmain' element={<Foodmain />}/>
+          </Route>
+
         </Routes>
       </BrowserRouter> 
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,63 @@
 import { Container, Navbar } from "react-bootstrap";
-import styled, { createGlobalStyle } from "styled-components";
+import Styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import FoodSidebar from "../features/FoodSidebar";
+
+const StyledHeader = Styled.div`
+height: 90px;
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #A8E6D6;
+
+p {
+  /* margin: 0 1rem;
+  font-size: 2rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #ffffff;
+  } */
+
+  margin: 0 1rem 0 0.8rem; // 오른쪽 1rem 왼쪽 0.8rem으로 수정
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #ffffff;
+  }
+}
+
+h3 {
+  margin-left: auto;
+  margin-right: 1rem;
+}
+`
+
+const MenuItems = Styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 120px; /* 메뉴들을 오른쪽으로 정렬 */
+`;
 
 function Header() {
 
-  const StyledHeader = styled.div`
-    height: 90px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #A8E6D6;
-    
-    p {
-      margin: 0 1rem;
-      font-size: 2rem;
-      cursor: pointer;
-
-      &:hover {
-        color: #ffffff;
-      }
-    }
-    
-    h3 {
-      margin-left: auto;
-      margin-right: 1rem;
-    }
-    `
-    
 
   return (
     <>
         <StyledHeader>
-          <p>메뉴1</p>
-          <p>메뉴2</p>
-          <p>메뉴3</p>
-          <p>메뉴4</p>
+          <FoodSidebar />
+            <MenuItems>
+              <p>메뉴1</p>
+              <p>메뉴2</p>
+              <p>메뉴3</p>
+              <p>메뉴4</p>
+            </MenuItems>
 
-          <h3>0000호</h3>
-        </StyledHeader>
+            <h3>0000호</h3>
+        </StyledHeader> 
+
         {/* <Navbar className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">menu1</Navbar.Brand>
