7237e18fbb22bb4876fd2aa02a78e3f85aee1a08
J-stone93 <treous11@gmail.com>
2024-06-20 18:01:44 +0900

    사이드바 만들기

---
+++ src/features/Health.jsx
@@ -0,0 +1,16 @@
+
+
+function Health() {
+  return (
+    <div>
+      <label>제목</label>
+      <input type="text" />
+      <label>내용</label>
+      <textarea name="" id=""></textarea>
+      <button>등록</button>
+
+    </div>
+  );
+};
+
+export default Health;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HealthList from './features/HealthList';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap CSS 추가
+import Sidebar from './features/Sidebar';
+import Health from './features/Health';
+// import 'react-toastify/dist/ReactToastify.min.css'; // ReactToastify CSS 추가
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <>
+    <Health />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
