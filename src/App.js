b76627ec91a79ad566cc00aedc8b17d42a229c95
Mabinum <Mabinum9528@naver.com>
2024-06-20 20:27:02 +0900

    BrowserRouter, GlobalStyle setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './pages/Main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+  body{
+    box-sizing: border-box;
+  }
+
+  * {
+    box-sizing: inherit;
+  }
+
+  .cursor-pointer{
+    cursor: pointer;
+  }
+`;
 
 function App() {
   return (
     <>
-      <Main/>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter> 
     </>
   );
 }
