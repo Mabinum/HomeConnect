f36569dd1b825eb02cc528c825a41b085093337d
Mabin <mabinum9528@naver.com>
2024-06-21 15:43:51 +0900

    up

--- src/App.js
+++ src/App.js
@@ -31,13 +31,14 @@ function App() {
       <GlobalStyle/>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main/>} />
-          <Route path='login' element={<Login/>}/>
-          <Route path='menu4' element={<Menu4/>}/>
-          {/* 나중에 메뉴바에 해당하는 것들 수정하기 */}
-          {/* <Route path='menu/:menuID' element={<MenuDetail/>}/> */}
-          {/* 나중에 params로 아이디에 맞는 메뉴화면 나오도록 하기 */}
-          {/* <Route path='health' element={<Health/>}/> */}
+          <Route path='/' element={<Layout/>} >
+            <Route path='login' element={<Login/>}/>
+            <Route path='menu4' element={<Menu4/>}/>
+            {/* 나중에 메뉴바에 해당하는 것들 수정하기 */}
+            {/* <Route path='menu/:menuID' element={<MenuDetail/>}/> */}
+            {/* 나중에 params로 아이디에 맞는 메뉴화면 나오도록 하기 */}
+            {/* <Route path='health' element={<Health/>}/> */}
+          </Route>
         </Routes>
       </BrowserRouter> 
     </>
