67762298ca536456b24afb70973bfc591518b617
pokm2360 <por0632@naver.com>
2024-06-21 13:49:47 +0900

    ㅇㅇ

--- src/features/main/mainSlice.js
+++ src/features/main/mainSlice.js
@@ -18,11 +18,7 @@ export const {getIDPWInfo} = mainSlice.actions;
 
 export const selectIDPWInfo = state => state.main.IDPWInfo;
 
-<<<<<<< HEAD
 export default mainSlice.reducer;
 
 // 메뉴 라우터구성
 // 데이터 받아오기
-=======
-export default mainSlice.reducer;
->>>>>>> 156f6cc93ff26c148055135ea4fdffc94cf88cac
