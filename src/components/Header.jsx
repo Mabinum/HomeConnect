4b2ad12b23d580a88e745215d1d8568c2f31efee
Mabin <mabinum9528@naver.com>
2024-06-21 14:15:37 +0900

    프로젝트 소개 및 역할 분담

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,18 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+
+const StyledNavbar = styled(Navbar.Brand)`
+  text-align: center;
+`;
+
 
 function Header() {
 
-
-  // const StyledHeader = styled.div`
-  //   height: 90px;
-  //   width: 100%;
-  //   display: flex;
-  //   justify-content: center;
-  //   align-items: center;
-  //   background-color: #A8E6D6;
-    
-  //   p {
-  //     margin: 0 1rem;
-  //     font-size: 2rem;
-  //     cursor: pointer;
-
-  //     &:hover {
-  //       color: #ffffff;
-  //     }
-  //   }
-    
-  //   h3 {
-  //     margin-left: auto;
-  //     margin-right: 1rem;
-  //   }
-  //   `
-    
-    const StyledNavbar = styled(Navbar.Brand)`
-      text-align: center;
-    `
+  const navigate = useNavigate();
 
   return (
     <>
-        {/* <StyledHeader>
-          <p>메뉴1</p>
-          <p>메뉴2</p>
-          <p>메뉴3</p>
-          <p>메뉴4</p>
-
-          <h3>0000호</h3>
-        </StyledHeader> */}
-
-      {/* <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
-
       <Navbar className="bg-body-tertiary">
         <Container>
           <StyledNavbar href="#">MENU1</StyledNavbar>
@@ -63,8 +22,11 @@ function Header() {
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              {/* <a href="#">000호</a> ????id값 전달 */}
-              입주자분 환영합니다.
+              {/* <a href="#">000호</a>
+              입주자분 환영합니다. */}
+              {/* 수정1 */}
+              <Nav.Link onClick={() => navigate('/')}>{}</Nav.Link>
+              {/* 마이페이지로 가게끔 나중에 주소 수정 */}
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
