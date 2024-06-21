1aaae9ad985656fd682c5997bb2dbf7864b7fee8
Mabin <mabinum9528@naver.com>
2024-06-21 12:46:15 +0900

    로그인 기능 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { Button, Form } from "react-bootstrap";
 import {Reset} from "styled-reset";
 import styled from "styled-components";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getIDPWInfo } from "../features/main/mainSlice";
 
 const Wrapper = styled.div`
 background-color: aqua;
@@ -58,30 +61,41 @@ const Container = styled.form`
 
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const [IDvalue, setIDvalue] = useState('');
+  const [PWvalue, setPWvalue] = useState('');
+
+  const handleIDChange = (e) => {
+    setIDvalue(e.target.value);
+  };
+
+  const handlePWChange = (e) => {
+    setPWvalue(e.target.value);
+  };
+
+  const handleSubmitINFO = () => {
+    dispatch(getIDPWInfo({ID : IDvalue , PW : PWvalue}))
+  };
+  
 
   return (
     <>
       <Reset/>
       <Wrapper>
         <Container>
-          {/* <label htmlFor="">
-            아이디 : <IdInput type="text" placeholder="username"/>
-          </label>
-          <label htmlFor="">
-            패스워드 : <PasswordInput type="password"/>
-          </label> */}
           <h1>
             로그인
           </h1>
           <div className="logindiv">
             <label htmlFor="loginId">아이디 :</label>
-            <Form.Control className="loginFunction" type="text" placeholder="username"/>
+            <Form.Control className="loginFunction" type="text" placeholder="username" value={IDvalue} onChange={handleIDChange}/>
           </div>
           <div className="logindiv">
             <label htmlFor="loginId">패스워드 :</label>
-            <Form.Control className="loginFunction" type="password" placeholder="password"/>
+            <Form.Control className="loginFunction" type="password" placeholder="password" value={PWvalue} onChange={handlePWChange}/>
           </div>
-          <Button type="button"> 로그인 </Button>
+          <Button type="button" onClick={handleSubmitINFO}> 로그인 </Button>
         </Container>
       </Wrapper>
     </>
