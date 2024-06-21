917e422d0dadedcf325d55f1dcc3e2f853476e81
junho <junho11_kr@naver.com>
2024-06-21 16:10:28 +0900

    24.06.21

--- src/App.js
+++ src/App.js
@@ -34,8 +34,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Main/>} />
           <Route path='login' element={<Login/>}/>
-          <Route path='menu4' element={<Menu4/>}/>
-          <Route path='Foodboard' element={<Foodmain />}/>
           {/* 나중에 메뉴바에 해당하는 것들 수정하기 */}
           {/* <Route path='menu/:menuID' element={<MenuDetail/>}/> */}
           {/* 나중에 params로 아이디에 맞는 메뉴화면 나오도록 하기 */}
