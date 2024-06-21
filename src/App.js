6723c09cc8cf96aa30073620f18383ea73105e2c
Mabin <mabinum9528@naver.com>
2024-06-21 16:07:04 +0900

    아바타 수정 및 메뉴바 네비게이션

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import Health from './features/Health';
 import HealthList from './features/HealthList';
 import Layout from './components/Layout';
 import Menu4 from './components/Menu4';
+import Menu1 from './components/Menu1';
+import Menu2 from './components/Menu2';
+import Menu3 from './components/Menu3';
+import MyPage from './components/MyPage';
 
 const GlobalStyle = createGlobalStyle`
   
@@ -33,7 +37,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Layout/>} >
             <Route path='login' element={<Login/>}/>
+            <Route path='menu1' element={<Menu1/>}/>
+            <Route path='menu2' element={<Menu2/>}/>
+            <Route path='menu3' element={<Menu3/>}/>
             <Route path='menu4' element={<Menu4/>}/>
+            <Route path='mypage' element={<MyPage/>}/>
             {/* 나중에 메뉴바에 해당하는 것들 수정하기 */}
             {/* <Route path='menu/:menuID' element={<MenuDetail/>}/> */}
             {/* 나중에 params로 아이디에 맞는 메뉴화면 나오도록 하기 */}
