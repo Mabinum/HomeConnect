f9ae396360f3b8c8862cb073a2f8603e6ef6910f
Mabin <mabinum9528@naver.com>
2024-06-21 12:19:06 +0900

    로그인 화면뷰어만 세팅

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap CSS 추가
+// import 'react-toastify/dist/ReactToastify.min.css'; // ReactToastify CSS 추가
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import { Button, Form, InputGroup } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import {Reset} from "styled-reset";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -7,39 +8,51 @@ position: fixed;
 width: 100%;
 height: 100%;
 display: flex;
-/* align-items: center;
-justify-content: center; */
+
+.loginFunction{
+  border: none;
+  border-radius: 3px;
+  outline: 1px solid blue;
+}
+
+.logindiv{
+  display: flex;
+  margin-top: 40px;
+}
+
+label {
+  width: 150px;
+  text-align : right ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  display: flex;
+  font-size: 50px;
+  position: absolute;
+  top: 130px;
+}
+
+Button {
+  margin-top: 50px;
+
+}
 `;
 
-const Container = styled.div`
+const Container = styled.form`
   display: flex;
-  width: 700px;
-  height: 900px;
+  width: 500px;
+  height: 700px;
   background-color: white;
   margin: auto;
   opacity: 0.9;
   border-radius: 3cap;
   align-items: center;
   justify-content: center;
-  /* flex-direction: column; */
-`;
-
-// const IdInput = styled(Form.Control)`
-//   margin-left: 10px;
-// `;
-
-// const PasswordInput = styled(Form.Control)`
-//   margin-left: 10px;
-// `;
-
-const IdInput = styled.input`
-  display: block;
-  margin-left: 10px;
-`;
-
-const PasswordInput = styled.input`
-  display: block;
-  margin-left: 10px;
+  flex-direction: column;
+  position: relative;
 `;
 
 
@@ -47,19 +60,31 @@ const PasswordInput = styled.input`
 function Login() {
 
   return (
-    <Wrapper>
-      <Container>
-        {/* <label htmlFor="">
-          아이디 : <IdInput type="text" placeholder="username"/>
-        </label>
-        <label htmlFor="">
-          패스워드 : <PasswordInput type="password"/>
-        </label> */}
-        아이디 : <IdInput type="text" placeholder="username"/>
-        패스워드 : <PasswordInput type="password"/>
-        <Button type="button"> 로그인 </Button>
-      </Container>
-    </Wrapper>
+    <>
+      <Reset/>
+      <Wrapper>
+        <Container>
+          {/* <label htmlFor="">
+            아이디 : <IdInput type="text" placeholder="username"/>
+          </label>
+          <label htmlFor="">
+            패스워드 : <PasswordInput type="password"/>
+          </label> */}
+          <h1>
+            로그인
+          </h1>
+          <div className="logindiv">
+            <label htmlFor="loginId">아이디 :</label>
+            <Form.Control className="loginFunction" type="text" placeholder="username"/>
+          </div>
+          <div className="logindiv">
+            <label htmlFor="loginId">패스워드 :</label>
+            <Form.Control className="loginFunction" type="password" placeholder="password"/>
+          </div>
+          <Button type="button"> 로그인 </Button>
+        </Container>
+      </Wrapper>
+    </>
   );
 };
 
