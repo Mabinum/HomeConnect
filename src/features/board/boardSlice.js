7d3e2267ae6f91f93cbdc6f931cf3051381e235d
J-stone93 <treous11@gmail.com>
2024-06-27 15:04:17 +0900

    24.06.27

--- src/features/board/boardSlice.js
+++ src/features/board/boardSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  foodList: [],
+  boardList: [],
   healthList: [],
   noticeList: [
     {
@@ -27,13 +27,17 @@ const boardSlice = createSlice({
   initialState,
   reducers: {
     Food: (state, action) => {
-      state.foodList.push({
-        id: action.payload.id,
-        title: action.payload.title,
-        content: action.payload.content,
-        date: action.payload.date 
-      });
+      console.log(action.payload);
+      state.boardList = action.payload;
     },
+    removeFoodList: (state, { payload: id }) => {
+      state.boardList = newList;
+      const newList = state.boardList.filter(list => list.id !== id);
+    },
+    // clearBoardList: (state) => {
+    //   state.boardList = null; 
+    // },
+
     HealthContent: (state, action) => {
       // console.log(action.payload.title);
       state.healthList.push({
@@ -54,10 +58,6 @@ const boardSlice = createSlice({
       const newList = state.healthList.filter(list => list.id !== id);
       state.healthList = newList;
     },
-    removeFoodList: (state, { payload: id }) => {
-      const newList = state.foodList.filter(list => list.id !== id);
-      state.foodList = newList;
-    },
   }
 });
 
@@ -66,10 +66,11 @@ export const {
   HealthContent,
   NoticeContent,
   removeHealthList,
-  removeFoodList
+  removeFoodList,
+  clearBoardList,
 } = boardSlice.actions;
 
-export const selectBoardSlice = state => state.board.foodList;
+export const selectBoardSlice = state => state.board.boardList;
 export const selectHealthInfo = state => state.board.healthList;
 export const selectNoticeInfo = state => state.board.noticeList;
 
--- src/features/food/BoardList.jsx
+++ src/features/food/BoardList.jsx
@@ -1,46 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Table, Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { selectBoardSlice } from '../board/boardSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Food, selectBoardSlice, clearBoardList } from '../board/boardSlice';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
+import BoardListDetail from './BoardListDetail';
 
 function BoardList() {
-    const foodList = useSelector(selectBoardSlice);
-    const navigate = useNavigate();
+	const boardList = useSelector(selectBoardSlice);
+	const navigate = useNavigate();
+	const dispatch = useDispatch()
 
-    const TableWrapper = styled(Table)`
+	const TableWrapper = styled(Table)`
         margin-top: 20px;
         width: 100%;
     `;
 
-    return (
-    <>
-        <Button variant="primary" onClick={()=>{navigate('/menu4/board')}}>게시글 작성</Button>
-        <TableWrapper>
-            <thead>
-                <tr>
-                    <th>게시물 번호</th>
-                    <th>제목</th>
-                    <th>작성일자</th>
-                    <th>작성자ID</th>
-                </tr>
-            </thead>
+	// 서버에서 boardlist 목록 요청
+	useEffect(() => {
+		// 서버에 상품 목록 요청
+		axios.get('http://localhost:8080/menu4/boardlist')
+			.then((response) => {
+				dispatch(Food(response.data));
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, [dispatch]);
 
-            {foodList.map((post, index) => (
-                <tbody onClick={() => navigate(`${post.id}`)}>
-                    <tr key={post.id}>
-                        <td>{index + 1}</td>
-                        <td>{post.title}</td>
-                        <td>{post.date}</td>
-                        <td>작성자 ID</td>
-                        {/* <td>{post.authorId}</td> */}
-                    </tr>
-                </tbody>
-            ))}
-        </TableWrapper>
-    </>
-    );
+	return (
+		<>
+			<Button variant="primary" onClick={() => { navigate('/menu4/board') }}>게시글 작성</Button>
+			<TableWrapper>
+				<thead>
+					<tr>
+						<th>게시물 번호</th>
+						<th>제목</th>
+						<th>작성일자</th>
+						<th>작성자ID</th>
+					</tr>
+				</thead>
+
+				{boardList.map((post) => (
+					<tbody onClick={() => {
+						navigate(`/menu4/boardlist/${post.writer}`)
+						}}>
+						<tr key={post.writer}>
+							<td>{post.no}</td>
+							<td>{post.title}</td>
+							<td>{post.regDate}</td>
+							<td>{post.writer}</td>
+							{/* <td>{post.authorId}</td> */}
+						</tr>
+					</tbody>
+				))}
+			</TableWrapper>
+		</>
+	);
 }
 
 export default BoardList;
