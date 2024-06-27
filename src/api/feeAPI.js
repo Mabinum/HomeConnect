518f9e599a3bce8dfc84f25003cbc0f8818be052
pokm2360 <por0632@naver.com>
2024-06-27 17:23:02 +0900

    dd

--- src/api/feeAPI.js
+++ src/api/feeAPI.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const addData = async () => {
   try {
     const response = await axios.post('http://localhost:8080/fee/register', { "userId":"aaa", "month":11, "water":20000, "electric":30000, "maintenance":10000 });
+    console.log(response);
     if (response.status === 201) {
       return response.data;
     } else {
