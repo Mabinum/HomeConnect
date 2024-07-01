f31421ecf37ffe7b7557779c3baaefb34fd96e2e
J-stone93 <treous11@gmail.com>
2024-07-01 18:15:37 +0900

    이미지 넘기고 받아오기

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainSlice from "../features/main/mainSlice";
 import boardSlice from "../features/board/boardSlice";
 import feeReducer from '../features/fee/feeSlice';
+import communitySlice from "../features/community/communitySlice";
 
 export const store = configureStore({
   reducer : {
     main : mainSlice,
     board : boardSlice,
     fees: feeReducer,
+    community: communitySlice,
   },
 });
